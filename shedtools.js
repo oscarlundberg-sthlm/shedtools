a25644c78646303f5833a119433595474740ea68
oscarlundberg-sthlm <oscarlundberg@hotmail.com>
2022-02-19 18:18:35 +0100

    color slider

--- shedtools.js
+++ shedtools.js
@@ -22,9 +22,65 @@ const createPseudoBody = () => {
     pseudoBody.style.pointerEvents = 'none';
 
     document.body.appendChild(pseudoBody);
+    return pseudoBody;
 }
-createPseudoBody();
-const shedToolsContainer = document.getElementById('shed_tools_-_pseudo_body');
+const shedToolsContainer = createPseudoBody();
+
+// Create bar for settings
+const createSettingsBar = () => {
+    let barEl = document.createElement('div');
+    let s = barEl.style;
+
+    s.background = '#202020';
+    s.color = '#EEEEEE';
+    s.position = 'fixed';
+    s.zIndex = 10000001;
+    s.bottom = 0;
+    s.left = 0;
+    s.width = '100%';
+    s.height = '3em';
+
+    const createColorSlider = () => {
+        let sliderEl = document.createElement('input');
+        sliderEl.type = 'range';
+        sliderEl.min = 0;
+        sliderEl.max = 360;
+        sliderEl.value = 180;
+        sliderEl.id = 'shed_tools_-_settings_-_color_slider';
+
+        return sliderEl;
+    }
+    const colorSlider = createColorSlider();
+
+    barEl.appendChild(colorSlider);
+    document.body.appendChild(barEl);
+
+    return barEl;
+}
+const settingsBar = createSettingsBar();
+
+
+let overlayColors = {
+    marginBox: `hsla(${180}, 100%, 50%, 0.6)`,
+    paddingBox: `hsla(${180 + 60}, 100%, 50%, 0.4)`,
+    contentBox: `hsla(${180 + 120}, 100%, 50%, 0.2)`,
+    setHue: function(num) {
+        let numB = 60;
+        let numC = numB * 2;
+        let pBoxHue = num + numB;
+        let cBoxHue = num + numC;
+
+        if (pBoxHue > 360) {
+            pBoxHue -= 360;
+        }
+        if (cBoxHue > 360) {
+            cBoxHue -= 360;
+        }
+        this.marginBox = `hsla(${num}, 100%, 50%, 0.6)`;
+        this.paddingBox = `hsla(${pBoxHue}, 100%, 50%, 0.4)`;
+        this.contentBox = `hsla(${cBoxHue}, 100%, 50%, 0.2)`;
+    }
+}
 
 const getOverlayEl = (currentTarget) => {
     const style = getComputedStyle(currentTarget);
@@ -49,20 +105,22 @@ const getOverlayEl = (currentTarget) => {
     wrapperEl.id = wrapperId;
 
     let marginBoxDiv = document.createElement('div');
-    marginBoxDiv.style.background = '#00FF0050';
+    marginBoxDiv.style.background = overlayColors.marginBox;
     marginBoxDiv.style.position = 'absolute';
     marginBoxDiv.style.top = '-' + marginTop;
     marginBoxDiv.style.right = '-' + marginRight;
     marginBoxDiv.style.bottom = '-' + marginBottom;
     marginBoxDiv.style.left = '-' + marginLeft;
     marginBoxDiv.innerText = 'm';
+    marginBoxDiv.style.transition = 'all .1s ease';
     
     let paddingBoxDiv = document.createElement('div');
     paddingBoxDiv.id = innerId;
-    paddingBoxDiv.style.background = '#FF000050';
+    paddingBoxDiv.style.background = overlayColors.paddingBox;
     paddingBoxDiv.style.width = currentTarget.clientWidth + 'px';
     paddingBoxDiv.style.height = currentTarget.clientHeight + 'px';
     paddingBoxDiv.innerText = '.p';
+    paddingBoxDiv.style.transition = 'all .1s ease';
     // paddingBoxDiv.style.pointerEvents = 'auto';
 
     let contentBoxDiv = document.createElement('div');
@@ -71,8 +129,9 @@ const getOverlayEl = (currentTarget) => {
     contentBoxDiv.style.right = paddingRight;
     contentBoxDiv.style.bottom = paddingBottom;
     contentBoxDiv.style.left = paddingLeft;
-    contentBoxDiv.style.background = '#0000FF50';
+    contentBoxDiv.style.background = overlayColors.contentBox;
     contentBoxDiv.innerText = '..c';
+    contentBoxDiv.style.transition = 'all .1s ease';
 
     wrapperEl.classList.add('shed_tools_-_overlay', 'shed_tools_-_overlay_wrapper');
     paddingBoxDiv.classList.add('shed_tools_-_overlay', 'shed_tools_-_overlay_inner');
@@ -84,6 +143,12 @@ const getOverlayEl = (currentTarget) => {
     return wrapperEl;
 }
 
+/**
+ * Handle overlay existence on mouse over
+ * @param {HTMLElement} a Current mouse over
+ * @param {HTMLElement} b Last mouse over
+ * @returns void
+ */
 const toggleOverlay = (a, b) => {
     if (a !== b) {
         if (a.tagName !== 'BODY') { 
@@ -141,3 +206,16 @@ document.body.addEventListener('keydown', e => {
         overlay.classList.toggle('shed_tools_-_locked_overlay');
     }
 })
+
+settingsBar.querySelector('#shed_tools_-_settings_-_color_slider')
+    .addEventListener('input', e => {
+        overlayColors.setHue(e.target.value);
+
+        let overlays = shedToolsContainer.children;
+        for (let i = 0; i < overlays.length; i++) {
+            let overlay = overlays.item(i);
+            overlay.children.item(0).style.background = overlayColors.marginBox;
+            overlay.children.item(1).style.background = overlayColors.paddingBox;
+            overlay.children.item(2).style.background = overlayColors.contentBox;
+        }
+    })
